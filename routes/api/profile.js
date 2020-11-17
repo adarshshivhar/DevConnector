8a8aa7d302658fc0946c3fac2a745376e7f3cebe
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-11-17 23:15:29 +0530

    Get Github Repos For Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,6 +13,10 @@ const User = require('../../models/User');
 //Express Validator
 const { check, validationResult } = require('express-validator');
 
+//For Github
+const request = require('request');
+const config = require('config');
+
 // ===================================================================== //
 // ===================================================================== //
 
@@ -234,19 +238,20 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     const profile = await Profile.findOne({ user: req.user.id });
 
     //Get remove index
-    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
 
     profile.experience.splice(removeIndex, 1);
 
     await profile.save();
 
     res.json(profile);
-
   } catch (err) {
-     console.error(err.message);
-     res.status(500).send('Server Error');
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
-})
+});
 
 // @route    PUT api/profile/education
 // @desc     ADD profile education
@@ -311,18 +316,51 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     const profile = await Profile.findOne({ user: req.user.id });
 
     //Get remove index
-    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
 
     profile.education.splice(removeIndex, 1);
 
     await profile.save();
 
     res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=pinned:asc&
+                  client_id=${config.get(
+                    'githubClientId'
+                  )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+
+      if(response.statusCode !== 200) {
+        res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body)); 
+    });
 
   } catch (err) {
-     console.error(err.message);
-     res.status(500).send('Server Error');
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
-})
+});
 
 module.exports = router;
